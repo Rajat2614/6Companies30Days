ae72f04255802f96a019a8eee253b598a845b7db
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-01-03 14:33:17 +0530

    Update Evaluate Reverse Polish Notation.cpp

--- Microsoft/Evaluate Reverse Polish Notation.cpp
+++ Microsoft/Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,30 @@
-
+int evalRPN(vector<string>& tok) {
+        stack<long long> st;
+        long long val;
+        for(int i=0;i<tok.size();i++){
+            if(tok[i]=="+" || tok[i]=="-" || tok[i]=="*" || tok[i]=="/"){
+                long long sec = st.top();
+                st.pop();
+                long long fir = st.top();
+                st.pop();
+                
+                if(tok[i] =="+")
+                    val = fir+sec;
+                
+                else if(tok[i]=="-")
+                    val = fir-sec;
+                
+                else if(tok[i]=="*")
+                    val = fir*sec;
+                
+                else if(tok[i]=="/")
+                    val = fir/sec;
+            }
+            else{
+                val = stoi(tok[i]);
+            }
+            st.push(val);
+        }
+        
+     return (int)st.top();
+}
