f2b8c0752fc11a06c2e2685cdade459726c3b68c
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-01-11 20:28:27 +0530

    Update Invalid Transactions.cpp

--- Goldman Sachs/Invalid Transactions.cpp
+++ Goldman Sachs/Invalid Transactions.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-        vector<string> invalidTransactions(vector<string>& transactions) {
-        unordered_set<string> res;
-        unordered_map<string,vector<vector<string>>> m;
-        for(auto& t:transactions){
-            istringstream ss(t);
-            vector<string> s(4,"");
-            int i=0;
-            while(getline(ss,s[i++],','));
-            if(stoi(s[2])>1000) res.insert(t);
-            for(vector<string> j:m[s[0]])
-                if((j[3]!=s[3])&&abs(stoi(j[1])-stoi(s[1]))<=60){
-                    res.insert(j[0]+","+j[1]+","+j[2]+","+j[3]);
-                    if(!res.count(t)) res.insert(t);
-                }
-            m[s[0]].push_back({s[0],s[1],s[2],s[3]});
-        }
+    vector<string> invalidTransactions(vector<string>& strr) {
+        int n = strr.size();
+        vector<vector<string>> arr;
+        vector<bool> check(n,true);
         vector<string> ans;
-        for(auto& k:res) ans.push_back(k);
+        
+        for(auto it : strr){
+            stringstream sstr(it);
+            vector<string> v;
+            while(sstr.good()){
+                string substr;
+                getline(sstr, substr, ',');
+                v.push_back(substr);
+            }
+            arr.push_back(v);
+        }
+        
+        sort(arr.begin(),arr.end(),[&](auto &a,auto &b) {return stoi(a[1]) < stoi(b[1]);});
+        
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<i;j++){
+                if(stoi(arr[j][1]) + 60 >= stoi(arr[i][1]) and arr[j][0] == arr[i][0] and arr[j][3] != arr[i][3]){
+                    check[j] = check[i] = false;
+                }
+            }
+            if(stoi(arr[i][2]) > 1000){
+                check[i] = false;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(!check[i]){
+                ans.push_back({arr[i][0] + "," + arr[i][1] + "," + arr[i][2] + "," + arr[i][3]});
+            }
+        }
+        
         return ans;
     }
 };
