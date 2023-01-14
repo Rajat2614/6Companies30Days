2f5e6f1499c031c89960485759ca8a6a69c47f7d
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-01-14 22:35:25 +0530

    Update Non-Negative Integers without Consecutive Ones.cpp

--- Adobe/Non-Negative Integers without Consecutive Ones.cpp
+++ Adobe/Non-Negative Integers without Consecutive Ones.cpp
@@ -1,21 +1,22 @@
 class Solution {
+    int calc(int len)
+    {
+        int l0 = 1, l1 = 0;
+        for (int i = 0; i < len; ++i)
+            l0 += l1, l1 = l0 - l1;
+        return l1 + l0;
+    }
 public:
     int findIntegers(int num) {
-        string str="";
-        while(num>0)str+=num%2+'0',num/=2;
-        int l=str.length();
-        vector<int> arr(l+1,1);
-        for(int i=2;i<=l;i++)arr[i]=arr[i-1]+arr[i-2];
-        int result=1,pre=0;
-        for(int i=l;i>=0;i--)
-        {
-            if(str[i-1]=='1')
+        int sum = 0;
+        bool is_last_one = false;
+        for (int i = 31; i >= 0; i --)
+            if ((1 << i & num) > 0)
             {
-                result+=arr[i];
-                if(pre==1)break;
+                sum += calc(i);
+                if ((1 << i+1 & num) > 0)
+                    return sum;
             }
-            pre=str[i-1]=='1';
-        }
-        return result-pre;
+        return sum + 1;
     }
 };
